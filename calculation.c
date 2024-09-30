1ce3ce69bb13b9e814ee0c83e0338fb75fccb586
likhith-15 <likhith2005kumar@gmail.com>
2024-09-30 14:28:26 +0530

    Create calculation.c

---
+++ calculation.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int add a,b;
+  printf("Enter the 2 numbers: ");
+  scanf("%d",&a);
+  scanf("%d",&b);
+  add = a+b;
+  printf("the sum is %d",add);
+  return 0;
+
+  
+}
